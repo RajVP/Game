68e6f3a706acefa2028ada8279146f859aa99fa2
Bawar Jalal <bawar96@hotmail.com>
2016-07-07 23:44:00 +0100

    pundi

--- ConsoleApplication1/main.cpp
+++ ConsoleApplication1/main.cpp
@@ -1,107 +1,238 @@
-#include <iostream>
-#include <conio.h>
-#include <Windows.h>
-using namespace std;
-bool gameOver;
-const int width = 20, height = 20;
-int x, y, foodX, foodY, score;
+//#include <iostream>
+//#include <conio.h>
+//#include <Windows.h>
+//using namespace std;
+//bool gameOver;
+//const int width = 20, height = 20;
+//int x, y, foodX, foodY, score;
+//int tailX[100], tailY[100];
+//int nTail; 
+//enum eDirection { STOP = 0, LEFT, RIGHT, DOWN, UP };
+//eDirection dir;
+//
+//void Setup()
+//{
+//	gameOver = false;
+//	dir = STOP;
+//	x = width / 2;
+//	y = height / 2;
+//	//Randomly places the food for the snake
+//	foodX = rand() % width;
+//	foodY = rand() % height;
+//	score = 0;
+//}
+//void Draw()
+//{
+//	//Clear the console window
+//	system("cls");
+//	//Prints the top wall
+//	for (int i = 0; i < width + 2; i++)
+//	{
+//		cout << "-";
+//	}
+//	cout << endl;
+//	for (int i = 0; i < height; i++)
+//	{
+//		for (int j = 0; j < height; j++)
+//		{
+//			//Prints the left wall
+//			if (j == 0)
+//				cout << "|";
+//			if (i == y && j == x)
+//				cout << "O";
+//			//Prints the food tile
+//			else if (i == foodY && j == foodX)
+//				cout << "F";
+//			else
+//			{
+//				//Prints the tail
+//				bool print = false;
+//				for (int k = 0; k < nTail; k++)
+//				{					
+//					if (tailX[k] == j && tailY[k] == i)
+//					{
+//						cout << "o";
+//						print = true;
+//					}
+//				}
+//				//Prints the spaces in between the walls
+//				if (!print)
+//					cout << " ";
+//			}
+//			//Print out the sides
+//			if (j == width - 1)
+//				cout << "|";
+//			
+//		}
+//		cout << endl;
+//	}
+//	//Print bottom row
+//	for (int i = 0; i < width + 2; i++)
+//	{
+//		cout << "-";
+//	}
+//	cout << endl;
+//	//Print score
+//	cout << "Score: " << score;
+//}
+//
+//void Input()
+//{
+//	//If keyboard key is pressed
+//	if (_kbhit())
+//	{
+//		//Get character
+//		switch (_getch())
+//		{
+//		case 'a':
+//			dir = LEFT;
+//			break;
+//		case 'd':
+//			dir = RIGHT;
+//			break;
+//		case 'w':
+//			dir = UP;
+//			break;
+//		case 's':
+//			dir = DOWN;
+//			break;
+//		case 'x':
+//			gameOver = true;
+//			break;
+//		}
+//	}
+//}
+//void Logic()
+//{
+//	int prevX = tailX[0];
+//	int prevY = tailY[0];
+//	int prev2X, prev2Y;
+//	//Tail is put in the position of the head
+//	tailX[0] = x;
+//	tailY[0] = y;
+//	//Constantly update the tail and head position
+//	for (int i = 1; i < nTail; i++)
+//	{
+//		prev2X = tailX[i];
+//		prev2Y = tailY[i];
+//		tailX[i] = prevX;
+//		tailY[i] = prevY;
+//		prevX = prev2X;
+//		prevY = prev2Y;
+//	}
+//	//Switch cases for character input
+//	switch (dir)
+//	{
+//	case LEFT:
+//		x--;
+//		break;
+//	case RIGHT:
+//		x++;
+//		break;
+//	case UP:
+//		y--;
+//		break;
+//	case DOWN:
+//		y++;
+//		break;
+//	default:
+//		break;
+//	}
+//	//If you want the walls to count as game over
+//	//if (x > width || x < 0 || y > height || y < 0)
+//		//gameOver = true;
+//
+//	//Pass through walls
+//	if (x >= width)
+//		x = 0;
+//	else if (x < 0)
+//		x = width - 1;
+//	if (y >= height)
+//		y = 0;
+//	else if (y < 0)
+//		y = height - 1;
+//	for (int i = 0; i < nTail; i++)
+//		if (tailX[i] == x && tailY[i] == y)
+//			gameOver = true;
+//	if (x == foodX && y == foodY)
+//	{
+//		score += 1;
+//		foodX = rand() % width;
+//		foodY = rand() % height;
+//		nTail++;
+//	}
+//}
+//
+//int main()
+//{
+//	Setup();
+//	while (!gameOver)
+//	{
+//		Draw();
+//		Input();
+//		Logic();
+//		//Slows down the game
+//		Sleep(75);
+//	}
+//
+//	return 0;
+//}
+//====================================================
+
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+#include <stdlib.h>
+
+bool gameOver = false;
+const int width = 500, height = 500;
+int x, y, foodX, foodY, score = 0;
 int tailX[100], tailY[100];
-int nTail; 
-enum eDirection { STOP = 0, LEFT, RIGHT, DOWN, UP };
-eDirection dir;
+int nTail;
+
+sf::RectangleShape head(sf::Vector2f(10, 10));
+sf::RectangleShape tail(sf::Vector2f(10, 10));
+sf::CircleShape food(10);
+sf::Font font;
+sf::Text text;
 
 void Setup()
 {
-	gameOver = false;
-	dir = STOP;
 	x = width / 2;
 	y = height / 2;
 	//Randomly places the food for the snake
 	foodX = rand() % width;
 	foodY = rand() % height;
-	score = 0;
 }
-void Draw()
+
+void Update()
 {
-	//Clear the console window
-	system("cls");
-	//Prints the top wall
-	for (int i = 0; i < width + 2; i++)
-	{
-		cout << "-";
-	}
-	cout << endl;
 	for (int i = 0; i < height; i++)
 	{
 		for (int j = 0; j < height; j++)
 		{
-			//Prints the left wall
-			if (j == 0)
-				cout << "|";
-			if (i == y && j == x)
-				cout << "O";
 			//Prints the food tile
-			else if (i == foodY && j == foodX)
-				cout << "F";
+			if (i == foodY && j == foodX)
+				food.setPosition(sf::Vector2f(foodX, foodY));
 			else
 			{
 				//Prints the tail
 				bool print = false;
 				for (int k = 0; k < nTail; k++)
-				{					
+				{
 					if (tailX[k] == j && tailY[k] == i)
 					{
-						cout << "o";
+						tail.setPosition(sf::Vector2f(i, j));
 						print = true;
 					}
 				}
-				//Prints the spaces in between the walls
-				if (!print)
-					cout << " ";
 			}
-			//Print out the sides
-			if (j == width - 1)
-				cout << "|";
-			
+
 		}
-		cout << endl;
 	}
-	//Print bottom row
-	for (int i = 0; i < width + 2; i++)
-	{
-		cout << "-";
-	}
-	cout << endl;
 	//Print score
-	cout << "Score: " << score;
+	text.setString("Score: 0" + score);
 }
 
-void Input()
-{
-	//If keyboard key is pressed
-	if (_kbhit())
-	{
-		//Get character
-		switch (_getch())
-		{
-		case 'a':
-			dir = LEFT;
-			break;
-		case 'd':
-			dir = RIGHT;
-			break;
-		case 'w':
-			dir = UP;
-			break;
-		case 's':
-			dir = DOWN;
-			break;
-		case 'x':
-			gameOver = true;
-			break;
-		}
-	}
-}
 void Logic()
 {
 	int prevX = tailX[0];
@@ -120,27 +251,10 @@ void Logic()
 		prevX = prev2X;
 		prevY = prev2Y;
 	}
-	//Switch cases for character input
-	switch (dir)
-	{
-	case LEFT:
-		x--;
-		break;
-	case RIGHT:
-		x++;
-		break;
-	case UP:
-		y--;
-		break;
-	case DOWN:
-		y++;
-		break;
-	default:
-		break;
-	}
+	
 	//If you want the walls to count as game over
 	//if (x > width || x < 0 || y > height || y < 0)
-		//gameOver = true;
+	//gameOver = true;
 
 	//Pass through walls
 	if (x >= width)
@@ -163,16 +277,66 @@ void Logic()
 	}
 }
 
+void Input() {
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+	{
+		head.move(-1, 0);
+	}
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+	{
+		head.move(1, 0);
+	}
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
+	{
+		head.move(0, -1);
+	}
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
+	{
+		head.move(0, 1);
+	}
+}
+
 int main()
 {
+	//////////init
 	Setup();
-	while (!gameOver)
+
+	sf::RenderWindow window(sf::VideoMode(500, 500), "Snake!");
+
+	food.setFillColor(sf::Color::Red);
+
+	head.setPosition(sf::Vector2f(x, y));
+
+	if (!font.loadFromFile("brickled.ttf"))
 	{
-		Draw();
+		// error...
+	}
+	text.setFont(font);
+	text.setCharacterSize(10); // in pixels, not points!
+	text.setPosition(sf::Vector2f(0, 490));
+	text.setColor(sf::Color::White);
+	
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+
+	//////////update
 		Input();
 		Logic();
-		//Slows down the game
-		Sleep(75);
+		Update();
+
+	//////////draw
+		window.clear();
+		window.draw(food);
+		window.draw(tail);
+		window.draw(head);
+		window.draw(text);
+		window.display();
 	}
 
 	return 0;
