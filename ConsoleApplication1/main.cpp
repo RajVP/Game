5dc31a0722b6b5572f503f49c8004952c19d512f
Bawar Jalal <bawar96@hotmail.com>
2016-07-13 14:48:31 +0100

    unnecesasry comments removed and code cleaned up

--- ConsoleApplication1/main.cpp
+++ ConsoleApplication1/main.cpp
@@ -1,184 +1,3 @@
-//#include <iostream>
-//#include <conio.h>
-//#include <Windows.h>
-//using namespace std;
-//bool gameOver;
-//const int width = 20, height = 20;
-//int x, y, foodX, foodY, score;
-//int tailX[100], tailY[100];
-//int nTail; 
-//enum eDirection { STOP = 0, LEFT, RIGHT, DOWN, UP };
-//eDirection dir;
-//
-//void Setup()
-//{
-//	gameOver = false;
-//	dir = STOP;
-//	x = width / 2;
-//	y = height / 2;
-//	//Randomly places the food for the snake
-//	foodX = rand() % width;
-//	foodY = rand() % height;
-//	score = 0;
-//}
-//void Draw()
-//{
-//	//Clear the console window
-//	system("cls");
-//	//Prints the top wall
-//	for (int i = 0; i < width + 2; i++)
-//	{
-//		cout << "-";
-//	}
-//	cout << endl;
-//	for (int i = 0; i < height; i++)
-//	{
-//		for (int j = 0; j < height; j++)
-//		{
-//			//Prints the left wall
-//			if (j == 0)
-//				cout << "|";
-//			if (i == y && j == x)
-//				cout << "O";
-//			//Prints the food tile
-//			else if (i == foodY && j == foodX)
-//				cout << "F";
-//			else
-//			{
-//				//Prints the tail
-//				bool print = false;
-//				for (int k = 0; k < nTail; k++)
-//				{					
-//					if (tailX[k] == j && tailY[k] == i)
-//					{
-//						cout << "o";
-//						print = true;
-//					}
-//				}
-//				//Prints the spaces in between the walls
-//				if (!print)
-//					cout << " ";
-//			}
-//			//Print out the sides
-//			if (j == width - 1)
-//				cout << "|";
-//			
-//		}
-//		cout << endl;
-//	}
-//	//Print bottom row
-//	for (int i = 0; i < width + 2; i++)
-//	{
-//		cout << "-";
-//	}
-//	cout << endl;
-//	//Print score
-//	cout << "Score: " << score;
-//}
-//
-//void Input()
-//{
-//	//If keyboard key is pressed
-//	if (_kbhit())
-//	{
-//		//Get character
-//		switch (_getch())
-//		{
-//		case 'a':
-//			dir = LEFT;
-//			break;
-//		case 'd':
-//			dir = RIGHT;
-//			break;
-//		case 'w':
-//			dir = UP;
-//			break;
-//		case 's':
-//			dir = DOWN;
-//			break;
-//		case 'x':
-//			gameOver = true;
-//			break;
-//		}
-//	}
-//}
-//void Logic()
-//{
-//	int prevX = tailX[0];
-//	int prevY = tailY[0];
-//	int prev2X, prev2Y;
-//	//Tail is put in the position of the rect
-//	tailX[0] = x;
-//	tailY[0] = y;
-//	//Constantly update the tail and rect position
-//	for (int i = 1; i < nTail; i++)
-//	{
-//		prev2X = tailX[i];
-//		prev2Y = tailY[i];
-//		tailX[i] = prevX;
-//		tailY[i] = prevY;
-//		prevX = prev2X;
-//		prevY = prev2Y;
-//	}
-//	//Switch cases for character input
-//	switch (dir)
-//	{
-//	case LEFT:
-//		x--;
-//		break;
-//	case RIGHT:
-//		x++;
-//		break;
-//	case UP:
-//		y--;
-//		break;
-//	case DOWN:
-//		y++;
-//		break;
-//	default:
-//		break;
-//	}
-//	//If you want the walls to count as game over
-//	//if (x > width || x < 0 || y > height || y < 0)
-//		//gameOver = true;
-//
-//	//Pass through walls
-//	if (x >= width)
-//		x = 0;
-//	else if (x < 0)
-//		x = width - 1;
-//	if (y >= height)
-//		y = 0;
-//	else if (y < 0)
-//		y = height - 1;
-//	for (int i = 0; i < nTail; i++)
-//		if (tailX[i] == x && tailY[i] == y)
-//			gameOver = true;
-//	if (x == foodX && y == foodY)
-//	{
-//		score += 1;
-//		foodX = rand() % width;
-//		foodY = rand() % height;
-//		nTail++;
-//	}
-//}
-//
-//int main()
-//{
-//	Setup();
-//	while (!gameOver)
-//	{
-//		Draw();
-//		Input();
-//		Logic();
-//		//Slows down the game
-//		Sleep(75);
-//	}
-//
-//	return 0;
-//}
-//====================================================
-
 #include <SFML/Graphics.hpp>
 #include <SFML/Window.hpp>
 #include <string>
@@ -200,7 +19,7 @@ RectangleShape food(Vector2f(50, 50));
 RectangleShape flash(Vector2f(width, height));
 Font font;
 Text text;
-bool foodIsBlocked;
+bool foodPlaceable;
 
 void Setup()
 {
@@ -222,18 +41,17 @@ void Update()
 	//collision between rect and food: increment score and randomise food position and increase speed of snake
 	if (tails.at(0).getGlobalBounds().intersects(food.getGlobalBounds()))
 	{
-		//randomPosition = Vector2f((rand() % int(width / food.getSize().x))*food.getSize().x, (rand() % int(height / food.getSize().y))*food.getSize().y);
-		foodIsBlocked = true;
+		foodPlaceable = true;
 
-		while (foodIsBlocked) {
+		while (foodPlaceable) {
 			randomPosition = Vector2f((rand() % int(width / food.getSize().x))*food.getSize().x, (rand() % int(height / food.getSize().y))*food.getSize().y);
 			for (i = 0; i < tails.size(); i++)
 				if (randomPosition == tails.at(i).getPosition()) {
-					foodIsBlocked = true;
+					foodPlaceable = true;
 					break;
 				}
 				else {
-					foodIsBlocked = false;
+					foodPlaceable = false;
 				}
 
 		}		
@@ -297,24 +115,6 @@ void Input() {
 int main()
 {
 	Setup();
-	//Randomly places the food for the snake
-	//randomPosition = Vector2f(rand() % width, rand() % height);
-	//randomPosition = Vector2f((rand() % int(width / food.getSize().x))*food.getSize().x, (rand() % int(height / food.getSize().y))*food.getSize().y);
-	
-	/*foodIsBlocked = true;
-
-	while (foodIsBlocked) {
-		randomPosition = Vector2f((rand() % int(width / food.getSize().x))*food.getSize().x, (rand() % int(height / food.getSize().y))*food.getSize().y);
-		for (i = 0; i < tails.size(); i++)
-			if (randomPosition == tails.at(i).getPosition()) {
-				foodIsBlocked = true;
-			}
-			else {
-				foodIsBlocked = false;
-				break;
-			}
-
-	}*/
 
 	food.setFillColor(Color::Red);
 
@@ -341,9 +141,8 @@ int main()
 
 		Input();
 		Update();
-
-		//food.setPosition(randomPosition);
-
+		
+		//move snake in pointing direction
 		tails.at(0).move(x, y);
 		for (i = tails.size() - 1; i >= 0; i--){
 			if (i != 0) {
@@ -351,6 +150,8 @@ int main()
 			}
 		}
 
+
+		//draw stuff
 		window.clear();
 		window.draw(food);
 		window.draw(text);
